e9fb319bb7be4e100dd82abd9573789cca6346e9
omni <omniprezenze@gmail.com>
2024-12-04 23:21:04 +0100

    explorer bookmarks, additional locale, default locale change

--- configs/base.nix
+++ configs/base.nix
@@ -103,9 +103,10 @@
   i18n.supportedLocales = [
     "en_US.UTF-8/UTF-8"
     "ru_RU.UTF-8/UTF-8"
+    "en_IE.UTF-8/UTF-8"
   ];
   
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_IE.UTF-8";
 
   users = {
     defaultUserShell = pkgs.zsh;
