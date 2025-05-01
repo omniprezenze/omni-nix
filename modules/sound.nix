f05b18368e47baaa8b00ab312b6e66c236933113
omni <omniprezenze@gmail.com>
2025-05-01 20:51:50 +0200

    moved monitors, bumped versions

--- modules/sound.nix
+++ modules/sound.nix
@@ -8,8 +8,15 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 64;
+        "default.clock.min-quantum" = 64;
+        "default.clock.max-quantum" = 64;
+      };
+    };
   };
-
   environment.systemPackages = with pkgs; [
     pavucontrol
   ];
