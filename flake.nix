0f8d523b62384ea79696942c5e6c3a28fc9d4f2a
omni <omniprezenze@gmail.com>
2026-08-30 18:27:46 +0200

    version bump

--- flake.nix
+++ flake.nix
@@ -13,7 +13,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-gaming.url = "github:fufexan/nix-gaming";
-    tuigreet.url = "github:NotAShelf/tuigreet"; # https://github.com/NotAShelf/tuigreet
   };
 
   outputs = { nixpkgs, ... }@inputs: {
@@ -23,9 +22,6 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/omnilaptop
-          { nixpkgs.overlays = [ (final: prev: {
-              tuigreet = inputs.tuigreet.packages.${prev.hostPlatform.system}.tuigreet;
-            }) ]; }
           #nixos-hardware.nixosModules.lenovo-legion-15ach6h
         ];
       };
@@ -37,16 +33,6 @@
           ./modules/systemd/default.nix
           ./modules/bin-scripts/default.nix
           ./modules/aagl.nix
-          {
-            nixpkgs.overlays = [
-              (final: prev: {
-                tuigreet = inputs.tuigreet.packages.${prev.stdenv.hostPlatform.system}.tuigreet;
-              })
-              (final: _prev: {
-                pnpm_10_29_2 = final.pnpm_10;
-              })
-            ];
-          }        
         ];
       };
     };
